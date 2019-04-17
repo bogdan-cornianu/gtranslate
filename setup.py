68e601fa9fb3b294107062b1b5fa4169dedb1d1d
Bogdan Cornianu <bogdan@bogdancornianu.com>
2019-04-17 20:28:23 +0300

    Change Python version to 3.6 in setup file

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(name='gtranslate',
       classifiers=[
         'Development Status :: 3 - Alpha',
         'License :: OSI Approved :: GPLv3',
-        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.6',
         'Topic :: Text Processing :: Linguistic',
       ],
       test_suite='nose.collector',
